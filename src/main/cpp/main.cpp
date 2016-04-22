058ecbdaba13f90f5243f8d03b48b68344b10d16
kepocnhh <kepocnhh@gmail.com>
2016-04-23 01:47:12 +0300

    make window stay on top

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,14 +1,18 @@
 #include "fox-1.6/fx.h"
+#include <windows.h>
 #include "Const.h"
 #include "ui/windows/MainWindow.h"
 
 int main(int argc, char *argv[])
 {
-  FXApp application("Hello", "FoxTest");
-  application.init(argc, argv);
-  MainWindow *main=new MainWindow(&application);
-  new FXButton(main, Const::app_name, NULL, &application, FXApp::ID_QUIT);
-  application.create();
-  main->show(PLACEMENT_SCREEN);
-  return application.run();
+    FXApp application("Hello", "FoxTest");
+    application.init(argc, argv);
+    MainWindow *main = new MainWindow(&application);
+    new FXButton(main, Const::app_name, NULL, &application, FXApp::ID_QUIT);
+    application.create();
+    main->show(PLACEMENT_SCREEN);
+    SetWindowPos((HWND)main->id(), HWND_TOPMOST,
+                 0, 0, 0, 0,
+                 SWP_NOMOVE | SWP_NOSIZE);
+    return application.run();
 }
--- src/main/cpp/ui/windows/MainWindow.cpp
+++ src/main/cpp/ui/windows/MainWindow.cpp
@@ -1,6 +1,6 @@
 #include "MainWindow.h"
 
 MainWindow::MainWindow(FXApp *a) : FXMainWindow(a, a->getAppName(), NULL, NULL,
-            DECOR_NONE, 0, 0, 100, 100)
+            DECOR_NONE | DECOR_TITLE, 0, 0, 100, 100)
 {
 }
